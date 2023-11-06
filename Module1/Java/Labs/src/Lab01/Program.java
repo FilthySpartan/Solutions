7cf6527fc130d43d4ff56a482481bcb011a43ae0
FilthySpartan <elliot_price@hotmail.com>
2023-11-06 12:36:16 +0000

    Created Java workspace

---
+++ Module1/Java/Labs/src/Lab01/Program.java
@@ -0,0 +1,10 @@
+package Lab01;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Ayyyyeeee Lmaoooooo");
+
+	}
+
+}
---
+++ Module1/Java/Program.java
@@ -0,0 +1,7 @@
+package Java;
+
+
+public class Program{
+public static void main(String[] args)
+ { System.out.println("My name is *chikka chikka* Slim Shady"); };
+}
